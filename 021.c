9466d4616aef3e9d3ca767f56283580ffdd23356
Tim Wilkens <timwilkensf1@gmail.com>
2014-05-31 11:27:15 -0700

    Factor out total code. Switch to pointers.

--- 021.c
+++ 021.c
@@ -3,13 +3,12 @@
 
 static int find_amicable(int n);
 static bool contains(int n, int *nums, int end);
+static int sum_array(int *nums, int end);
 
 int
 main(void) {
-
-    int total = 0;
     int amicables[100];
-    int index = 0;
+    int *p = amicables;
     int length = sizeof(amicables)/sizeof(amicables[0]);
 
     for (int i = 1; i <= 10000; i++) {
@@ -20,19 +19,27 @@ main(void) {
         int b = find_amicable(a);
         if (i == b) {
             if (!contains(i, amicables, length)) {
-                amicables[index++] = i;
-                total += i;
+                *p++ = i;
             }
 
             if (!contains(b, amicables, length)) {
-                amicables[index++] = b;
-                total += b;
+                *p++ = b;
             }
         }
     }
  
+    int total = sum_array(amicables, length);
     printf("%d\n", total);
     return 0;
+}
+
+static int 
+sum_array(int *nums, int end) {
+    int total = 0;
+    for (int i = 0; i < end && nums[i] != 0; i++) {
+        total += nums[i];
+    }
+    return total;
 }
 
 static bool
