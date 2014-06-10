52d333aacd6dca9f1ffbf98db9f9d72352c0fb25
Tim Wilkens <timwilkensf1@gmail.com>
2014-06-10 08:27:57 -0700

    Updated syntax.

--- 002.hs
+++ 002.hs
@@ -3,10 +3,8 @@ fib 1 = 1
 fib 2 = 2
 fib n = fib(n - 1) + fib(n - 2)
 
-isEven n = if mod n 2 == 0 then True else False
-
 -- Slow because we re-compute fib for each number.
-total = sum (filter isEven (takeWhile (< 4000000) (map fib [1..])))
+total = sum $ filter  (\x -> x `mod` 2 == 0) $ takeWhile (< 4000000) $ map fib [1..]
 
 main = do
   putStrLn(show total)
