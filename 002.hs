b25eef5062ce76d1e07992f9632e2f3fb430a175
Tim Wilkens <timwilkensf1@gmail.com>
2014-06-10 08:53:22 -0700

    Quite a bit faster now.

--- 002.hs
+++ 002.hs
@@ -1,10 +1,5 @@
-fib :: Int -> Int
-fib 1 = 1
-fib 2 = 2
-fib n = fib(n - 1) + fib(n - 2)
-
--- Slow because we re-compute fib for each number.
-total = sum $ filter  (\x -> x `mod` 2 == 0) $ takeWhile (< 4000000) $ map fib [1..]
+fib = 1 : 1 : [ a+b | (a,b) <- zip fib (tail fib) ]
+total = sum $ filter  (\x -> x `mod` 2 == 0) $ takeWhile (< 4000000) fib
 
 main = do
   putStrLn(show total)
