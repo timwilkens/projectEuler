0dd1d7d024e3ff183979684ee39bce1a2f833f98
Tim Wilkens <timwilkensf1@gmail.com>
2014-06-13 08:21:42 -0700

    Fast solution to problem 10.

---
+++ 010.hs
@@ -0,0 +1,22 @@
+-- Taken from this Paper (Solution due to Richard Bird)
+-- http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+
+primes = 2:([3..] `minus` composites)
+  where
+    composites = union [multiples p | p <- primes]
+
+multiples n = map (n*) [n..]
+
+(x:xs) `minus` (y:ys) | x < y  = x:(xs `minus` (y:ys))
+                      | x == y = xs `minus` ys
+                      | x > y  = (x:xs) `minus` ys
+
+union = foldr merge []
+  where
+    merge (x:xs) ys = x:merge' xs ys
+    merge' (x:xs) (y:ys) | x < y  = x:merge' xs (y:ys)
+                         | x == y = x:merge' xs ys
+                         | x > y  = y:merge' (x:xs) ys
+
+main = do
+  putStrLn $ show $ sum $ takeWhile (<= 2000000) primes
